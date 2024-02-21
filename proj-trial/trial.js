238f846b8b74eac704b645aef5ede2c628af5c3a
Annarhysa Albert <annarhysa13@gmail.com>
2024-02-21 10:50:59 +0530

    LEarning a few more concepts and changed the file name

---
+++ proj-trial/trial.js
@@ -0,0 +1,35 @@
+// Log a statment using console.log()
+console.log('Hello from Anna')
+
+//Learning to use variables
+let age = 22
+console.log(age)
+
+//adding variable using let is dynamic
+
+//values of const cannot be changed
+const salary = 10101
+console.log(salary)
+
+//learning about loops
+const a = 4;
+
+if(a>5){
+    console.log("Greater");
+}
+else{
+    console.log("Smaller");
+}
+
+for(let i = 0; i<5; i++){
+    console.log(i);
+}
+
+//learning about functions
+
+//traditional method
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(5,3));
